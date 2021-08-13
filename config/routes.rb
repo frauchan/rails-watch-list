84f8bb6b3f6a03e5c5c43746cf695b7503b462aa
Christine Chan <christine.u.chan@gmail.com>
2021-08-13 15:54:18 +0200

    complete lists stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:new, :create, :index, :show] do
+  end
 end
-
-# A user can see all the lists
-# GET "lists"
-# A user can see the details of a given list and its name
-# GET "lists/42"
-# A user can create a new list
-# GET "lists/new"
-# POST "lists"
