dff16cc363c2bb7e7610627a239d653ef507fffd
Christine Chan <christine.u.chan@gmail.com>
2021-08-13 12:27:28 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-A user can see all the lists
-GET "lists"
-A user can see the details of a given list and its name
-GET "lists/42"
-A user can create a new list
-GET "lists/new"
-POST "lists"
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
